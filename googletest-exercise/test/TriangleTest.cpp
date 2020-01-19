661785cda23ecaf39be25e979cf3648e994cc694
Emily Chan <emc15@sfu.ca>
2020-01-19 12:57:23 -0800

    part 1 done

--- googletest-exercise/test/TriangleTest.cpp
+++ googletest-exercise/test/TriangleTest.cpp
@@ -7,20 +7,43 @@
 //     public:
 //     //some mock tests
 // };
+
+
 shapes::Triangle tri{5,4,3};
 
-TEST(TriangleTests, getPerimeter)
+//FAILED TESTS===========================================
+
+TEST(TriangleTests, getPerimeterFail)
 {
     EXPECT_EQ(12, tri.getPerimeter());
 }
 
-
-TEST(TriangleTests, getArea)
+TEST(TriangleTests, getAreaFail)
 {
     EXPECT_EQ(6, tri.getArea());
 }
 
-TEST(TriangleTests, getKind)
+TEST(TriangleTests, getKindFail)
 {
-    // EXPECT_EQ(Kind SCALENE, tri.getKind);
+    // shapes::Triangle::Kind::SCALENE
+    EXPECT_EQ(shapes::Triangle::Kind::EQUILATERAL, tri.getKind());
 }
+
+//PASSED TESTS===========================================
+
+TEST(TriangleTests, getPerimeterPass)
+{
+    EXPECT_EQ(13, tri.getPerimeter());
+}
+
+TEST(TriangleTests, getAreaPass)
+{
+//    std::cout << tri.getArea();
+    EXPECT_DOUBLE_EQ(9.2364766009555836, tri.getArea());
+}
+
+TEST(TriangleTests, getKindPass)
+{
+    EXPECT_EQ(shapes::Triangle::Kind::SCALENE, tri.getKind());
+}
+
