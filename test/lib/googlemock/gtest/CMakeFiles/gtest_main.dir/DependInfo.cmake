eda2d4ee03b864e4f76c61894fb2b45553d8bc1b
Emily Chan <emc15@sfu.ca>
2020-01-18 18:13:51 -0800

    first commit

---
+++ test/lib/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/emleechxn/Desktop/473/assg1/googletest-exercise/test/lib/googletest/src/gtest_main.cc" "/Users/emleechxn/Desktop/473/assg1/test/lib/googlemock/gtest/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest-exercise/include"
+  "googletest-exercise/test/lib/googletest/include"
+  "googletest-exercise/test/lib/googlemock/include"
+  "googletest-exercise/test/lib/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/emleechxn/Desktop/473/assg1/test/lib/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
